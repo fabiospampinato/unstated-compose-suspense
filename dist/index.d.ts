1f14c1e9ddadf64d0770c999568ea450be0524c9
Fabio Spampinato <spampinabio@gmail.com>
2018-11-04 18:56:27 +0100

    Ignored dist folder

--- dist/index.d.ts
+++
@@ -1,9 +0,0 @@
-import { Container } from 'unstated-suspense';
-declare class ChildContainer<Context extends object, State extends object> extends Container<State> {
-    ctx: Context;
-}
-declare class ParentContainer<Context extends object, State extends object> extends Container<State> {
-    ctx: Context;
-    [index: string]: Container<State>[keyof Container<State>] | Context[keyof Context] | Context;
-}
-export { ChildContainer, ParentContainer };
